5508dd2d41b2ff88aa6d4490f9e6f7fb060c171a
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-04 15:21:04 +0200

    implemented gateway constructor

--- src/Gateway/gateway.d
+++ src/Gateway/gateway.d
@@ -5,5 +5,50 @@ const ubyte GATEWAY_VERSION = 6;
 
 class Gateway
 {
-    
+    Client client;
+
+    WebSocket webSock;
+
+    string sessionID;
+
+    uint sequenceNumber;
+
+    uint heartbeatInterval;
+
+    bool connected;
+
+    uint reconnects;
+
+    Task heartbeater;
+
+    Emitter eventEmitter;
+
+    private string cachedGatewayURL;
+    private Couunter!string eventCounter;
+    private bool eventTracking;
+
+    this(Client client, bool eventTracking = false)
+    {
+        this.client = client;
+        this.eventTracking = eventTracking;
+
+        this.eventEmitter = new Emitter;
+        this.eventEmitter.listen!Ready(toDelegate(&this.handleReadyEvent));
+        this.eventEmitter.listen!Resumed(toDelegate(&this.handleResumedEvent);
+
+        //client.events = this.eventEmitter;
+
+        if (this.eventTracking) {
+            this.eventCounter = new Counter!string;
+        }
+    }
+
+    void start()
+    {
+        if(this.webSock && this.webSock.connected) this.webSock.close();
+
+        // TODO: Set gateway url.. 
+        this.sock = connectWebSocket(URL(this.cachedGatewayURL));
+        runTask(toDelegate(&this.run));
+    }
 }
--- src/client.d
+++ src/client.d
@@ -1,6 +1,24 @@
 module brokendiscord.client;
 
-class Client 
+abstract class Client 
 {
-    
+    /* Client authentication token. */
+    string token;
+
+    ShardInfo* shardInfo;
+
+    Gateway gateway;
+
+    this(string token, ShardInfo* shardInfo = null) 
+    {
+        this.token = token;
+        this.shardInfo = shardInfo ? shardInfo : new ShardInfo();
+
+        this.gateway = new Gateway(this);
+    }
+
+    public void run()
+    {
+
+    }
 }
