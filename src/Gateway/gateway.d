5f7f8c59c56c1d0a54d8337f944ad3bd0ffba24e
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-04 14:37:00 +0200

    Initial project structure

---
+++ src/Gateway/gateway.d
@@ -0,0 +1,9 @@
+module brokendiscord.gateway.gateway;
+
+/* Gateway version */
+const ubyte GATEWAY_VERSION = 6;
+
+class Gateway
+{
+    
+}
---
+++ src/client.d
@@ -0,0 +1,6 @@
+module brokendiscord.client;
+
+class Client 
+{
+    
+}
