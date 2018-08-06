df2c3178f0a63b6e7fe733db883aff6fb5f224fa
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-07 00:21:27 +0200

    Reimplementing Gateway connection without aspnet deps

--- BrokenDiscord/Gateway.fs
+++ BrokenDiscord/Gateway.fs
@@ -1,12 +1,12 @@
 module BrokenDiscord.Gateway
 
 open System
+open System.Text
 open System.Threading
 open System.Threading.Tasks
 open System.Net.WebSockets
 open Microsoft.AspNetCore.Http
 open Newtonsoft.Json.Linq
-open System.Text
 
 // OP = opcode 
 // d = event data
@@ -31,42 +31,62 @@ type OpCode =
 type Gateway () =
     let GATEWAY_VERSION = 6
 
-    let mutable sockets = list<WebSocket>.Empty
-
-    let addSocket sockets socket = socket :: sockets
-
-    let removeSocket sockets socket =
-        sockets
-        |> List.choose (fun s -> if s <> socket then Some s else None)
-    
-    let sendMessage =
-        fun (socket : WebSocket) (message : string) ->
-            let buffer = Encoding.UTF8.GetBytes(message)
-            let segment = new ArraySegment<byte>(buffer)
-
-            if socket.State = WebSocketState.Open then
-                do socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None) |> ignore
-            else
-                sockets <- removeSocket sockets socket
-
-    member this.connect (context : HttpContext) =
+    let Run (uri : string) = 
         async {
-            if context.Request.Path = PathString("wss://gateway.discord.gg/?v=6&encoding=json") then
-                match context.WebSockets.IsWebSocketRequest with
-                | true ->
-                    let! websocket = context.WebSockets.AcceptWebSocketAsync() |> Async.AwaitTask
-                    sockets <- addSocket sockets websocket
+            let webSocket = new ClientWebSocket()
+            webSocket.ConnectAsync(new Uri(uri), CancellationToken.None) |> Async.AwaitTask |> ignore
+            
+            // Recieve Hello packet
 
-                    let buffer : byte[] = Array.zeroCreate 4096
-                    let! ct = Async.CancellationToken
-                    
-                    websocket.ReceiveAsync(new ArraySegment<byte>(buffer), ct)
-                        |> Async.AwaitTask
-                        |> ignore
-                    
-                    let content = Encoding.UTF8.GetString buffer
-                    printf "%s" content
+            // Start heartbeat task
 
+            // Send indentification
 
-                | false -> context.Response.StatusCode <- 400
         } |> Async.StartAsTask :> Task
+
+    let Send = 
+        0
+    
+    let Recieve =
+        0
+
+
+    //let mutable sockets = list<WebSocket>.Empty
+
+    //let addSocket sockets socket = socket :: sockets
+
+    //let removeSocket sockets socket =
+    //    sockets
+    //    |> List.choose (fun s -> if s <> socket then Some s else None)
+    
+    //let sendMessage =
+    //    fun (socket : WebSocket) (message : string) ->
+    //        let buffer = Encoding.UTF8.GetBytes(message)
+    //        let segment = new ArraySegment<byte>(buffer)
+
+    //        if socket.State = WebSocketState.Open then
+    //            do socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None) |> ignore
+    //        else
+    //            sockets <- removeSocket sockets socket
+
+    //member this.connect (context : HttpContext) =
+    //    async {
+    //        if context.Request.Path = PathString("wss://gateway.discord.gg/?v=6&encoding=json") then
+    //            match context.WebSockets.IsWebSocketRequest with
+    //            | true ->
+    //                let! websocket = context.WebSockets.AcceptWebSocketAsync() |> Async.AwaitTask
+    //                sockets <- addSocket sockets websocket
+
+    //                let buffer : byte[] = Array.zeroCreate 4096
+    //                let! ct = Async.CancellationToken
+                    
+    //                websocket.ReceiveAsync(new ArraySegment<byte>(buffer), ct)
+    //                    |> Async.AwaitTask
+    //                    |> ignore
+                    
+    //                let content = Encoding.UTF8.GetString buffer
+    //                printf "%s" content
+
+
+    //            | false -> context.Response.StatusCode <- 400
+    //    } |> Async.StartAsTask :> Task
