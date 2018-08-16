73a9cde040c57bb44321b4c8221245841304930b
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-16 02:26:26 +0200

    Added NUnit test support to BrokenDiscord.Test project.

--- BrokenDiscord.Test/Program.fs
+++ BrokenDiscord.Test/Program.fs
@@ -1,14 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+module Program  
+    let [<EntryPoint>] main _ = 0
 
-open BrokenDiscord.Client
-open BrokenDiscord.Gateway
-open System
+//open System
+//open BrokenDiscord
+//open BrokenDiscord.Client
+//open BrokenDiscord.Gateway
 
-[<EntryPoint>]
-let main argv = 
-    let c = new Client(OnReady = 
-        (fun args -> printf "%s" "\n\n This is the client responding to the OnReady Event \n\n"))
-    c.login()
-    Console.ReadLine() |> ignore
-    0 // return an integer exit code
+//[<EntryPoint>]
+//let main argv = 
+//    let c = new Client(OnReady = 
+//        (fun args -> printf "%s" "\n\n This is the client responding to the OnReady Event \n\n"))
+//    c.login()
+//    Console.ReadLine() |> ignore
+//    0 // return an integer exit code
---
+++ BrokenDiscord.Test/Test.fs
@@ -0,0 +1,11 @@
+﻿namespace BrokenDiscord.Test
+
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type TestClass () =
+
+    [<Test>]
+    member this.TestMethodPassing() =
+        Assert.True(true)
