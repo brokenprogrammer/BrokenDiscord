9b637a1f65453cb72608281295aed111eed89294
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-19 08:12:08 +0200

    Added documentation for the WebSocket module.

--- BrokenDiscord/WebSocket.fs
+++ BrokenDiscord/WebSocket.fs
@@ -1,11 +1,11 @@
 ﻿namespace BrokenDiscord.WebSockets
 
-open System.Net.WebSockets
 
 module WebSocket =
     open System
-    open System.Threading
     open System.IO
+    open System.Net.WebSockets
+    open System.Threading
 
     /// Size of the buffer when sending messages over the socket.
     type BufferSize = int
@@ -15,22 +15,26 @@ module WebSocket =
     [<Literal>]
     let defaultBufferSize : BufferSize = 16384
 
+    /// Wrapper for the RecieveAsync function, takes a buffer and target socket.
     let receive (buffer : ArraySegment<byte>) (socket : ClientWebSocket) = 
         async {
             let! result = socket.ReceiveAsync(buffer, CancellationToken.None) |> Async.AwaitTask
             return result
         }
-
+    
+    /// Wrapper for the SendAsync function, takes a buffer, message type and endOfMessage and target socket.
     let send (buffer : ArraySegment<byte>) messageType endOfMessage (socket : ClientWebSocket) =
         async {
             do! socket.SendAsync(buffer, messageType, endOfMessage, CancellationToken.None) |> Async.AwaitTask
         }
-
+    
+    /// Gracefull approach to closing the socket. This tells the other end that the socket is being closed.
     let close status message (socket : ClientWebSocket) =
         async {
             do! socket.CloseAsync(status, message, CancellationToken.None) |> Async.AwaitTask
         }
     
+    /// Sends a message to the specified socket from the specified stream.
     let sendMessage bufferSize messageType (stream : IO.Stream) (socket : ClientWebSocket) = 
         async {
             let buffer = Array.create (bufferSize) Byte.MinValue
@@ -47,13 +51,16 @@ module WebSocket =
                 }
             do! sendMessage'()
         }
-
+    
+    /// Sends a specified UTF8 string as the message for specified socket.
     let sendMessageUTF8 (text : string) (socket : ClientWebSocket) = 
         async {
             let stream = new IO.MemoryStream(Text.Encoding.UTF8.GetBytes text)
             do! sendMessage defaultBufferSize WebSocketMessageType.Text stream socket
         }
-
+    
+    /// Receives a message and writes it to the specified stream
+    /// Attempts to handle closes gracefully
     let receieveMessage cancellationToken bufferSize messageType (writeableStream : IO.Stream) (socket : ClientWebSocket) =
         async {
             let buffer = new ArraySegment<Byte>(Array.create (bufferSize) Byte.MinValue)
@@ -77,7 +84,8 @@ module WebSocket =
                 }
             do! recieveTilEnd' ()
         }
-
+    
+     /// Receives a message as an UTF8 string from specified socket.
     let receieveMessageUTF8 (socket : ClientWebSocket) = 
         async {
             let stream = new IO.MemoryStream()
