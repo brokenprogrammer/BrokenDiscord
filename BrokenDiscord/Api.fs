bbbfa80b3d06aca7233789b7439fa7749fd1d4f4
Oskar Mendel <brokenprogrammer@gmail.com>
2018-08-27 02:06:18 +0200

    Abstracted post and get HTTP requests in Api.fs

--- BrokenDiscord/Api.fs
+++ BrokenDiscord/Api.fs
@@ -5,29 +5,46 @@ open System.Net
 open System.Net.Http
 open System.Net.Http.Headers
 open BrokenDiscord.Types
+open System.Text
+
+//TODO: This should be set on type created and hidden from api user.
+let setHeaders (client : HttpClient) (token : string) (userAgent : string) = 
+    //client.DefaultRequestHeaders.Accept.Clear()
+    //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"))
+    client.DefaultRequestHeaders.Add("Authorization", String.Format("Bot {0}", token))
+    client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+    //client.DefaultRequestHeaders.Add("Content-Type", "application/json")
 
 type Api (token : string) =
-    let client : HttpClient = new HttpClient()
-
     let baseURL = "https://discordapp.com/api"
-    let userAgent = String.Format("DiscordBot ($url, $versionNumber)", "https://github.com/brokenprogrammer/BrokenDiscord", "6") //TODO FORMAT
-    let token = token 
+    let userAgent = String.Format("DiscordBot ($url, $versionNumber)", "https://github.com/brokenprogrammer/BrokenDiscord", "6")
+    let token = token
+    
+    let client : HttpClient = new HttpClient()
+    do setHeaders client token userAgent
 
-    //TODO: Abstract GetAsync function
-    member this.getChannel (id : Snowflake) = 
+    member this.GET path =
         async {
-            let! res = client.GetAsync(baseURL + String.Format("/channels/{0}", id)) |> Async.AwaitTask
-            if res.IsSuccessStatusCode <> true then
-                printf "Error"
-            
-            let! channel = res.Content.ReadAsStringAsync() |> Async.AwaitTask
-            printf "CHANNEL FROM API: %s" channel
-            return channel
+            let! res = client.GetAsync(baseURL + path) |> Async.AwaitTask
+            if res.IsSuccessStatusCode then
+                let! content = res.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return Some content
+            else
+                return None
         }
-
-    //TODO: This should be set on type created and hidden from api user.
-    member this.setheaders = 
-        client.DefaultRequestHeaders.Accept.Clear()
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"))
-        client.DefaultRequestHeaders.Add("Authorization", String.Format("Bot {0}", token))
-        client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+    
+    member this.POST path content =
+        async {
+            let! res = client.PostAsync((baseURL + path), StringContent(content, Encoding.UTF8, "application/json")) |> Async.AwaitTask
+            if res.IsSuccessStatusCode then
+                let! content = res.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return Some content
+            else
+                return None
+        }
+    
+    member this.PUT path =
+        0
+    
+    member this.DELETE path =
+        0
