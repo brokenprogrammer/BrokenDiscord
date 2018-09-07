b43f39b67634ace0ff363ded2eef503f0e5e9fff
алый <iseurie@gmail.com>
2018-09-07 15:29:07 -0400

    Examples!

---
+++ BrokenDiscord.Examples/Ping/Ping.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open BrokenDiscord.Client
+open BrokenDiscord.Events
+open BrokenDiscord.Types
+open Hopac
+open Hopac.Infixes
+
+let token = sprintf "Bot %s" <| Environment.GetEnvironmentVariable "PING_BOT_TOKEN"
+let client = new Client(token)
+
+let pong (m : Message) =
+    job {
+        if not <| Option.defaultValue false m.author.bot && m.content = "!ping" then
+            return!
+                client.CreateMessage m.channelId <| MessageCreate.T.New "pong!"
+                |> Job.startIgnore
+        else return ()
+    } |> ignore
+
+[<EntryPoint>]
+let main _argv =
+    let token = sprintf "Bot %s" <| Environment.GetEnvironmentVariable "PING_BOT_TOKEN"
+    let client = new Client(token)
+    client.subscribe () |> Async.RunSynchronously
+    client.Events
+    |> Event.choose (function MessageCreate e -> Some e | _ -> None)
+    |> Event.add pong
+    0
