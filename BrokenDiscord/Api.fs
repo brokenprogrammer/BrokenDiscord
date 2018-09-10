cf7ac3cab95a6bf0dcf947e00196ce7e9f17fa3f
алый <iseurie@gmail.com>
2018-09-10 04:49:15 -0400

    Revert "Ratelimiting + chessie"
    This reverts commit e119eb868076539a8b3c01081c59b80f0c2ec9fe.
    
    # Conflicts:
    #	BrokenDiscord/Api.fs
    #	BrokenDiscord/Client.fs

--- BrokenDiscord/Api.fs
+++ BrokenDiscord/Api.fs
@@ -7,10 +7,9 @@ open BrokenDiscord.Types
 open BrokenDiscord.Json.Json
 open Newtonsoft.Json
 
-open Hopac
 open HttpFs.Client
-open Chessie.Hopac.JobTrial
-open Chessie.ErrorHandling
+
+open Hopac
 open Hopac.Infixes
 
 let private userAgent =
@@ -25,100 +24,16 @@ let private setHeaders token req =
 
 let private basePath = sprintf "https://discordapp.com/api/v6/%s"
 
-module Ratelimiting =
-    open Hopac
-    type Target = Global | Route of string
-    type Cessation = 
-        { target : Target; release : DateTime }
-        with
-        member x.Timeout =
-            let span = x.release - DateTime.Now
-            if span < TimeSpan(0L) then Alt.zero () else timeOut span
-            
-    let private inbox = new Ch<Cessation> ()
-    let mutable private cache = Map.empty
-    let private bulletin = new Event<Cessation> ()
-    let cessationNotifier =
-        job {
-            while true do
-                let! cessation = Ch.take inbox
-                let { target=target; release=release } = cessation
-                cache <- Map.add target release cache
-                bulletin.Trigger cessation
-                do! cessation.Timeout
-                cache <- Map.remove target cache
-        }
-    let ceased = cache
-    let cessations = bulletin.Publish
-    let notify x =
-        new Promise<unit>(Ch.send inbox x)
-        |> Promise.read <|> x.Timeout
 
-open Ratelimiting
-    
-type RatelimError =
-    { [<JsonProperty "retry_after">]
-      retry_ms : uint32
-      [<JsonProperty "global">]
-      _global : bool }
-      
-module RatelimError =
-    let cessation route x = 
-        {   target = 
-                if x._global then Ratelimiting.Target.Global
-                else Ratelimiting.Target.Route route
-            release = 
-                float x.retry_ms
-                |> TimeSpan.FromMilliseconds
-                |> (+) DateTime.Now
-        }
- 
 module Response =
-    open Chessie.Hopac
+    let parseRtn<'t> r = 
+        let s = Response.readBodyAsString r
+        if r.statusCode - 200 < 100 then s >>- ofJson<'t> >>- Ok
+        else s >>- ofJson<ApiError> >>- Error
 
-    let rateCk r =
-        if r.statusCode = 429 then
-            let route =
-                let path =
-                    r.responseUri.Segments 
-                    |> Seq.map (String.filter ((<>) '/'))
-                    |> String.concat "/"
-                sprintf "%s/%s" r.responseUri.Host path
-            let cessation =
-                Response.readBodyAsString r >>- ofJson<RatelimError>
-                >>- RatelimError.cessation route
-            cessation >>= Ratelimiting.notify |> Job.startIgnore |> ignore
-            cessation >>- FSharp.Core.Result.Error
-        else Job.result <| FSharp.Core.Ok r
-    
-    let errCk r =
-        if r.statusCode - 200 < 100 && r.statusCode <> 429 then
-            Job.result <| Result.Succeed r
-        else Response.readBodyAsString r 
-                >>- ofJson<ApiError>
-                >>- (fun x -> (r.statusCode, x))
-                >>- Result.FailWith
-    
-    let rec rateGuard req = 
-        job {
-            let! rsp = getResponse req >>= rateCk
-            match rsp with
-                | FSharp.Core.Ok rsp -> return rsp
-                | Error cessation -> 
-                    do! notify cessation |> Job.startIgnore
-                    do! cessation.Timeout
-                    return! rateGuard req
-        }
-        
-    let parseRtn<'t> r =
-        rateGuard r >>= errCk |> ofJobOfResult
-        |> mapFun (Response.readBodyAsString>>run>>ofJson<'t>)
-        |> toJobOfResult
-
-    let parseStat = 
-        rateGuard >> Job.bind errCk
-        >> ofJobOfResult >> mapFun ignore
-        >> toJobOfResult
+    let parseStat r =
+        if r.statusCode - 200 < 100 then Job.result <| Ok ()
+        else Response.readBodyAsString r >>- ofJson<ApiError> >>- Error
 
 module Request =
     let jsonBody<'t> (x : 't option) =
@@ -134,18 +49,19 @@ module Request =
         Request.createUrl method <| basePath path
         |> setHeaders token
         |> jsonBody<'i> body
+        |> getResponse
         
     let call<'i, 'o> token method path body =
-        enqueue<'i> token method path body |> Response.parseRtn<'o>
+        enqueue<'i> token method path body >>= Response.parseRtn<'o>
 
     let thunk<'i> token method path (body : 'i option) =
-        enqueue<'i> token method path body |> Response.parseStat
+        enqueue<'i> token method path body >>= Response.parseStat
 
 let restForm<'t> token method path (data : FormData list) =
     Request.createUrl method <| basePath path
     |> setHeaders token
     |> Request.body (RequestBody.BodyForm data)
-    |> Response.parseRtn<'t>
+    |> getResponse >>= Response.parseRtn<'t>
 
 // these expect return values from the cloud.
 let restGetCall<'i, 'o>    = Request.call<'i, 'o> /> Get
